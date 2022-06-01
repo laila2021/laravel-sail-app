969be0f5966cb4f9f321a86b019c7b980f86e7e5
laila2021 <71378724+laila2021@users.noreply.github.com>
2022-06-01 21:31:02 +0300

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,15 +3,16 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-        <h1>
-       Welcome, I'm Laila Al-Kainae
-        </h1>
+        
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                <h1>
+               <b>Welcome, I'm Laila Al-Kainae</b>
+                </h1>
                     You're logged in!
                 </div>
             </div>
